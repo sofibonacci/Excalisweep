d0fb973e1425a43e3d50a029d62d1d11bab08e24
lulotrix <101540565+lulotrix@users.noreply.github.com>
2025-03-31 10:10:54 -0300

    relocate text output
    relocate text output from show_billed_services to show_intro

--- main.py
+++ main.py
@@ -15,6 +15,12 @@ def show_intro():
     print("Warning: The resources displayed are based on your current Availability Zone (AZ) and Region. If you're unable to find what you're looking for, try switching to a different AZ or Region."       )
     print(f'Warning: The current variable for real deletion is set on {delete_for_real}. Either way, be careful!')
 
+    region = get_region()
+    az = get_availability_zone()
+    print(f"📍 Region: {region}")
+    print(f"🏠 Availability Zone: {az}\n")
+    
+
 def get_region():
     """Get current AWS region."""
     session = boto3.session.Session()
@@ -55,17 +61,13 @@ def list_billed_services():
 def show_billed_services():
     """Display AWS services that have incurred costs in the last specified number of days."""
     print(f"\nAWS Services with costs in the last {days_to_observe} days:")
-    region = get_region()
-    az = get_availability_zone()
-    print(f"📍 Region: {region}")
-    print(f"🏠 Availability Zone: {az}\n")
 
     services = list_billed_services()
     if not services:
         print("No services with costs found.")
     else: 
         for service, cost in sorted(services.items(), key=lambda x: x[1], reverse=True):
-            print(f" {service}: ${cost:.2f} (Region: {region}, AZ: {az})")
+            print(f" {service}: ${cost:.2f}")
 
 def invoke_script(script_name):
     """Execute a cleanup wizard script safely."""
