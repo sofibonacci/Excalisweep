f6e0d41b7bdd422fdf2e3f46c80a4018d7d534b6
martinatoffoletto <99765115+martinatoffoletto@users.noreply.github.com>
2025-03-31 10:27:45 -0300

    k

--- wizards/other_services_wizard.py
+++ wizards/other_services_wizard.py
@@ -115,12 +115,17 @@ def execute_method(service_name, method_name): #execute the method u choose (and
             except Exception as e:
                     print(f"Error executing method: {e}")  
         
-        print(type(response), response.keys())            
-        print_list_enumerate(response,"Response: ")
+        
+        for item in response.keys():
+            print(item)
+        if response["ResponseMetadata"]["HTTPStatusCode"]== 200:
+            print_list_enumerate(response,"Response: ")
+        else: 
+            print(response["ResponseMetadata"])
+            
         print(response)
                 
-                
-            
+           
     except Exception as e:
         print(f"{e}")
 
