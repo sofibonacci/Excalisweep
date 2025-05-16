bc0dc9c6588d84b67702bad783d68ce56a0e98ee
martinatoffoletto <99765115+martinatoffoletto@users.noreply.github.com>
2025-05-16 09:41:06 -0300

    fix

--- utility.py
+++ utility.py
@@ -138,7 +138,7 @@ def select_from_list(item_list, prompt_message, allow_all=True):
 def run_interactive_menu(title, options):
     print(f"""
     *****************************************
-       {title}   
+    {title}   
     *****************************************
     """)
 
