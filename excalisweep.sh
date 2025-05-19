516b23fc8f8ffec79ff9225f8472666fc09d9983
Lourdes Micaela Suarez <101540565+lulotrix@users.noreply.github.com>
2025-05-19 10:16:29 -0300

    194 (#35)
    * Create bash file
    
    execute with -> bash excalisweep.sh
    
    * changed time to improve user exp

---
+++ excalisweep.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#search and execute test scripts
+
+echo "executing tests, wait a minute..."
+sleep 1
+
+python -m unittest discover -s tests -p "*unittest.py"
+
+echo "tests done!"
+sleep 2
+
+#execute app
+python main.py
