7010bee085c9e07e2817056f90abf81aa8e8e939
Lourdes Micaela Suarez <101540565+lulotrix@users.noreply.github.com>
2025-05-26 13:09:16 -0300

    201 (#44)
    * formatting tests on bash file
    
    for each test, print ok or failed with file name
    
    * print to see error
    
    * fix script calling as module instead of path
    
    * fix import module test.test_fixtures
    
    * fix access to module
    
    -with "from tests.test_fixtures" to access to path.
    -fix typo on cloud_formation test script "from text_fixtures" to " from tests.test_fixtures"
    
    * deleted echo with output to improve user exp
    
    * show output only when test failed

--- excalisweep.sh
+++ excalisweep.sh
@@ -2,13 +2,33 @@
 
 #search and execute test scripts
 
-echo "executing tests, wait a minute..."
+echo "executing tests wait a minute..."
+echo ""
 sleep 1
-
-python -m unittest discover -s tests -p "*unittest.py"
+test_dir="tests"
+ok_count=0
+fail_count=0
+#finds every script that ends with "unittest.py"
+find "$test_dir" -type f -name "*unittest.py" | while read -r test_file; do
+    # Converts file in a module to use with python -m
+    module_name=$(echo "$test_file" | sed 's|/|.|g' | sed 's|\.py$||')    
+    output=$(python -m unittest "$module_name" 2>&1)
+    status=$?
+    if [ $status -eq 0 ]; then
+        echo "================$test_file: OK================"
+        ((ok_count++))
+    else
+        echo "================$test_file: FAILED================"
+        ((fail_count++))   
+        echo "$output"
+    fi
+    echo ""    
+    sleep 1
+done
 
 echo "tests done!"
-sleep 2
-
-#execute app
+sleep 1 
+echo "executing Excalisweep..."
+sleep 1
+#executing main...
 python main.py
